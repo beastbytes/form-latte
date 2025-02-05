34d41c56b1686708491a2e7455ab98ab0245bab0
Chris <chris@clyates.plus.com>
2025-02-05 22:30:27 +0000

    Update tests

--- tests/FormExtensionTest.php
+++ tests/FormExtensionTest.php
@@ -5,48 +5,51 @@ declare(strict_types=1);
 namespace BeastBytes\View\Latte\Form\Tests;
 
 use BeastBytes\View\Latte\Form\FormExtension;
+use BeastBytes\View\Latte\Form\Tests\Support\TestForm;
 use BeastBytes\View\Latte\LatteFactory;
-use BeastBytes\View\Latte\Form\Tests\Support\Form\TextForm;
 use Generator;
 use Latte\Engine;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Files\FileHelper;
+use Yiisoft\FormModel\FormModel;
 
 class FormExtensionTest extends TestCase
 {
-    private string $cacheDir = __DIR__ . '/public/tmp';
+    private const  CACHE_DIR = __DIR__ . '/generated/cache';
+    private const  TEMPLATE_DIR = __DIR__ . '/generated/template';
+
     private Engine $latte;
 
     protected function setUp(): void
     {
         parent::setUp();
-        FileHelper::ensureDirectory($this->cacheDir);
-        $factory = new LatteFactory($this->cacheDir, extensions: [new FormExtension()]);
+        FileHelper::ensureDirectory(self::CACHE_DIR);
+        FileHelper::ensureDirectory(self::TEMPLATE_DIR);
+        $factory = new LatteFactory(self::CACHE_DIR, extensions: [new FormExtension()]);
         $this->latte = $factory->create();
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
-        //FileHelper::removeDirectory($this->cacheDir);
+ //       FileHelper::removeDirectory(self::CACHE_DIR);
+ //       FileHelper::removeDirectory(self::TEMPLATE_DIR);
     }
     
     #[Test]
-    public function field(): void
+    #[DataProvider('fieldTagProvider')]
+    public function field(string $tag, string $result): void
     {
-        $formModel = new TextForm();
+        $formModel = new TestForm();
+        $this->createFieldTemplate($tag, $formModel);
 
-        $result = $this
+        $field = $this
             ->latte
-            ->renderToString(__DIR__ . '/public/views/text-field.latte', ['formModel' => $formModel]);
+            ->renderToString(self::TEMPLATE_DIR . "/$tag-field.latte", ['formModel' => $formModel]);
 
-        $this->assertNotEmpty($result);
-        $this->assertStringContainsString('<input type="text" id="textform-framework"', $result);
-        $this->assertStringContainsString('<input type="text" id="textform-engine"', $result);
-        $this->assertStringContainsString('maxlength="25"', $result);
-        $this->assertStringContainsString('minlength="5"', $result);
+        $this->assertSame($result, $field);
     }
 
     #[Test]
@@ -61,7 +64,7 @@ class FormExtensionTest extends TestCase
     }
 
     #[Test]
-    #[DataProvider('tagProvider')]
+    #[DataProvider('fieldTagProvider')]
     public function getTags(string $tag): void
     {
         $extension = new FormExtension();
@@ -82,8 +85,139 @@ class FormExtensionTest extends TestCase
         yield ['filter' => 'target'];
     }
 
-    public static function tagProvider(): Generator
+    public static function fieldTagProvider(): Generator
     {
-        yield ['tag' => 'text'];
+        /*
+        yield [
+            'tag' => 'checkbox',
+            'result' => '<input type="text" id="testform-text"">',
+        ];
+        yield [
+            'tag' => 'checkboxList',
+            'result' => '<input type="text" id="testform-text">',
+        ];
+        */
+        yield [
+            'tag' => 'date',
+            'result' => "<div>\n"
+                . '<label for="testform-date">Date Field</label>' . "\n"
+                . '<input type="date" id="testform-date" name="TestForm[date]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        /*
+        yield [
+            'tag' => 'dateTimeLocal',
+            'result' => "<div>\n"
+                . '<label for="testform-time">Time Field</label>' . "\n"
+                . '<input type="tel" id="testform-time" name="TestForm[time]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        */
+        yield [
+            'tag' => 'email',
+            'result' => "<div>\n"
+                . '<label for="testform-email">Email Field</label>' . "\n"
+                . '<input type="email" id="testform-email" name="TestForm[email]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        yield [
+            'tag' => 'file',
+            'result' => "<div>\n"
+                . '<label for="testform-file">File Field</label>' . "\n"
+                . '<input type="file" id="testform-file" name="TestForm[file]">' . "\n"
+                . '</div>'
+            ,
+        ];
+        yield [
+            'tag' => 'hidden',
+            'result' => '<input type="hidden" id="testform-hidden" name="TestForm[hidden]" value>',
+        ];
+        yield [
+            'tag' => 'image',
+            'result' => "<div>\n"
+                . '<label for="testform-image">Image Field</label>' . "\n"
+                . '<input type="image" id="testform-image" name="TestForm[image]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        yield [
+            'tag' => 'number',
+            'result' => "<div>\n"
+                . '<label for="testform-number">Number Field</label>' . "\n"
+                . '<input type="number" id="testform-number" name="TestForm[number]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        yield [
+            'tag' => 'password',
+            'result' => "<div>\n"
+                . '<label for="testform-password">Password Field</label>' . "\n"
+                . '<input type="password" id="testform-password" name="TestForm[password]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        /*
+        yield [
+            'tag' => 'radioList',
+            'result' => '',
+        ];
+        yield [
+            'tag' => 'range',
+            'result' => '',
+        ];
+        yield [
+            'tag' => 'select',
+            'result' => '',
+        ];
+        */
+        yield [
+            'tag' => 'telephone',
+            'result' => "<div>\n"
+                . '<label for="testform-telephone">Telephone Field</label>' . "\n"
+                . '<input type="tel" id="testform-telephone" name="TestForm[telephone]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        yield [
+            'tag' => 'text',
+            'result' => "<div>\n"
+                . '<label for="testform-text">Text Field</label>' . "\n"
+                . '<input type="text" id="testform-text" name="TestForm[text]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        /*
+        yield [
+            'tag' => 'textarea',
+            'result' => '<textarea id="testform-textarea" name="TestForm[textarea]>',
+        ];
+        */
+        yield [
+            'tag' => 'time',
+            'result' => "<div>\n"
+                . '<label for="testform-time">Time Field</label>' . "\n"
+                . '<input type="time" id="testform-time" name="TestForm[time]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+        yield [
+            'tag' => 'url',
+            'result' => "<div>\n"
+                . '<label for="testform-url">Url Field</label>' . "\n"
+                . '<input type="url" id="testform-url" name="TestForm[url]" value>' . "\n"
+                . '</div>'
+            ,
+        ];
+    }
+
+    private function createFieldTemplate($tag)
+    {
+        $template = "{varType Yiisoft\\FormModel\\FormModel \$formModel}\n\n";
+        $template .= '{' . $tag . " \$formModel, '$tag'}";
+
+        file_put_contents(self::TEMPLATE_DIR . "/$tag-field.latte", $template);
     }
 }
---
+++ tests/Support/TestForm.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeastBytes\View\Latte\Form\Tests\Support;
+
+use Yiisoft\FormModel\FormModel;
+
+class TestForm extends FormModel
+{
+    private string $checkbox = '';
+    private string $checkboxList = '';
+    private string $date = '';
+    private string $dateTimeLocal = '';
+    private string $email = '';
+    private string $file = '';
+    private string $hidden = '';
+    private string $image = '';
+    private string $number = '';
+    private string $password = '';
+    private string $radioList = '';
+    private string $range = '';
+    private string $select = '';
+    private string $telephone = '';
+    private string $text = '';
+    private string $textarea = '';
+    private string $time = '';
+    private string $url = '';
+
+    public function getPropertyLabels(): array
+    {
+        return [
+            'checkbox' => 'Checkbox Field',
+            'checkboxList' => 'CheckboxList Field',
+            'date' => 'Date Field',
+            'dateTimeLocal' => 'DateTimeLocal Field',
+            'email' => 'Email Field',
+            'file' => 'File Field',
+            'hidden' => 'Hidden Field',
+            'image' => 'Image Field',
+            'number' => 'Number Field',
+            'password' => 'Password Field',
+            'radioList' => 'RadioList Field',
+            'range' => 'Range Field',
+            'select' => 'Select Field',
+            'telephone' => 'Telephone Field',
+            'text' => 'Text Field',
+            'textarea' => 'Textarea Field',
+            'time' => 'Time Field',
+            'url' => 'Url Field',
+        ];
+    }
+}
