e0f2ce3a509cc1463b82f631c1f6a7ca09346d9b
Chris <chris@clyates.plus.com>
2025-02-09 23:29:31 +0000

    Support counter config settings (currently only tabIndex) and validation rules enrichment

--- src/Config/ConfigTrait.php
+++ src/Config/ConfigTrait.php
@@ -6,24 +6,49 @@ namespace BeastBytes\View\Latte\Form\Config;
 
 use Latte\Compiler\Nodes\Php\FilterNode;
 use Latte\Compiler\Nodes\Php\ModifierNode;
+use Yiisoft\FormModel\ValidationRulesEnricher;
 
 trait ConfigTrait
 {
     protected ?ModifierNode $config = null;
+    private string $enrich = 'enrich';
+    private static array $counters = [
+        'tabIndex',
+    ];
 
     private function getConfig($context): string
     {
+        static $counters = [
+            'tabIndex' => 1,
+        ];
+
         $configuration = [];
 
         /** @var FilterNode $config */
         foreach ($this->config as $config) {
             $args = [];
+            $name = $config->name->name;
 
             foreach ($config->args as $arg) {
                 $args[] = $arg->print($context);
             }
 
-            $configuration[] = "'" . $config->name->name . "()'=>[" . join(',', $args) . ']';
+            if ($name === $this->enrich) {
+                $configuration[] = "'enrichFromValidationRules()'=>[true]";
+
+                if (count($args) === 0) {
+                    $cls = 'new Yiisoft\FormModel\ValidationRulesEnricher()';
+                } else {
+                    $cls = 'new ' . str_replace(["'", '\\\\'], ['', '\\'], $args[0]) . '()';
+                }
+                $configuration[] = "'validationRulesEnricher()'=>[$cls]";
+            } else {
+                if (in_array($name, self::$counters) && count($args) === 0) {
+                    $args[] = $counters[$name]++;
+                }
+
+                $configuration[] = "'" . $name . "()'=>[" . join(',', $args) . ']';
+            }
         }
 
         return '[' . join(',', $configuration) . ']';
