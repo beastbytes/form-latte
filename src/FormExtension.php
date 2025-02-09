72b4e89df053972496403f036b6c3c15d8622167
Chris <chris@clyates.plus.com>
2025-02-09 14:14:57 +0000

    Remove filters Add more tags

--- src/FormExtension.php
+++ src/FormExtension.php
@@ -9,48 +9,6 @@ use Latte\Extension;
 
 final class FormExtension extends Extension
 {
-    public function getFilters(): array
-    {
-        return [
-            'accept' => Config\Field::value(...),
-            'alt' => Config\Field::value(...),
-            'ariaDescribedBy' => Config\Field::variadic(...),
-            'ariaLabel' => Config\Field::value(...),
-            'autofocus' => Config\Field::boolean(...),
-            'buttons' => Config\Field::variadic(...),
-            'cols' => Config\Field::value(...),
-            'dirname' => Config\Field::value(...),
-            'disabled' => Config\Field::boolean(...),
-            'enrich' => Config\Field::enrich(...),
-            'form' => Config\Field::value(...),
-            'height' => Config\Field::value(...),
-            'inputInvalidClass' => Config\Field::value(...),
-            'inputValidClass' => Config\Field::value(...),
-            'invalidClass' => Config\Field::value(...),
-            'items' => Config\Field::value(...),
-            'list' => Config\Field::value(...),
-            'max' => Config\Field::value(...),
-            'maxlength' => Config\Field::value(...),
-            'min' => Config\Field::value(...),
-            'minlength' => Config\Field::value(...),
-            'multiple' => Config\Field::boolean(...),
-            'optionsData' => Config\Field::value(...),
-            'pattern' => Config\Field::value(...),
-            'placeholder' => Config\Field::value(...),
-            'readonly' => Config\Field::boolean(...),
-            'required' => Config\Field::boolean(...),
-            'rows' => Config\Field::value(...),
-            'size' => Config\Field::value(...),
-            'src' => Config\Field::value(...),
-            'step' => Config\Field::value(...),
-            'tabIndex' => Config\Field::counter(...),
-            'usePlaceholder' => Config\Field::boolean(...),
-            'validClass' => Config\Field::value(...),
-            'width' => Config\Field::value(...),
-            'wrap' => Config\Field::value(...),
-        ];
-    }
-
     public function getTags(): array
     {
         return [
@@ -58,6 +16,7 @@ final class FormExtension extends Extension
             'buttonGroup' => Node\GroupNode::create(...),
             'checkbox' => Node\FieldNode::create(...),
             'checkboxList' => Node\FieldNode::create(...),
+            'color' => Node\FieldNode::create(...),
             'date' => Node\FieldNode::create(...),
             'dateTimeLocal' => Node\FieldNode::create(...),
             'email' => Node\FieldNode::create(...),
@@ -67,17 +26,24 @@ final class FormExtension extends Extension
             'form' => Node\FormNode::create(...),
             'hidden' => Node\FieldNode::create(...),
             'image' => Node\ButtonNode::create(...),
+            'month' => Node\FieldNode::create(...),
             'number' => Node\FieldNode::create(...),
             'password' => Node\FieldNode::create(...),
+            'radio' => Node\FieldNode::create(...),
             'radioList' => Node\FieldNode::create(...),
             'range' => Node\FieldNode::create(...),
+            'reset' => Node\ButtonNode::create(...),
             'resetButton' => Node\ButtonNode::create(...),
+            'search' => Node\FieldNode::create(...),
             'select' => Node\FieldNode::create(...),
+            'submit' => Node\ButtonNode::create(...),
             'submitButton' => Node\ButtonNode::create(...),
+            'tel' => Node\FieldNode::create(...),
             'telephone' => Node\FieldNode::create(...),
             'text' => Node\FieldNode::create(...),
             'textarea' => Node\FieldNode::create(...),
             'time' => Node\FieldNode::create(...),
+            'week' => Node\FieldNode::create(...),
             'url' => Node\FieldNode::create(...),
         ];
     }
@@ -85,18 +51,5 @@ final class FormExtension extends Extension
     public function getCacheKey(Engine $engine): string
     {
         return md5(self::class);
-    }
-
-    public static function getConfig(array $configuration): array
-    {
-        $config = [];
-
-        foreach ($configuration as $c) {
-            foreach ($c as $k => $v) {
-                $config[$k] = $v;
-            }
-        }
-
-        return $config;
     }
 }
